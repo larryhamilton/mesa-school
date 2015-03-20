c4ca1a67b8781b95f5e322b5de623566e89a4924
larryhamilton <lhamilton@yopmail.com>
2015-03-20 14:38:43 -0700

    update to xke

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -259,13 +259,13 @@
     
 // ORIGINAL    $scope.radioHost = 'http://192.99.8.192'; // Replace this with your own radio stream URL
 // ORIGINAL    $scope.radioPort = '3536'; // Replace this with the port of your Radio Stream
-    $scope.radioHost = 'http://108.163.223.242/stream'; // Replace this with your own radio stream URL
-    $scope.radioPort = '8139'; // Replace this with the port of your Radio Stream
+    $scope.radioHost = 'http://13.bteradio.com:9090/963xke'; // Replace this with your own radio stream URL
+    $scope.radioPort = '9090'; // Replace this with the port of your Radio Stream
     $scope.lastFMKey = 'e3343637ee292e488adf1e92bceb3a4f';
     $scope.lastFM = 'http://ws.audioscrobbler.com/2.0/?method=track.search&format=json&limit=1&api_key='+$scope.lastFMKey+'&track=';
 
 // ORIGINAL    $scope.radioURL = $scope.radioHost+':'+$scope.radioPort+'/;';
-    $scope.radioURL = 'http://108.163.223.242:8139/stream';  
+    $scope.radioURL = 'http://13.bteradio.com:9090/963xke';  
     $scope.buttonIcon = '<span class="ion-ios-play"></span>';
 
     $scope.radioOptions = {
